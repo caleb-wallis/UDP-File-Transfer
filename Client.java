8f24fa718469a057449d720ec7b70c06d2e9892a
cw500 <cw500@students.waikato.ac.nz>
2025-05-20 05:33:30 +1200

    Sends file download request

--- Client.java
+++ Client.java
@@ -1,45 +1,51 @@
 import java.net.*;
 import java.util.Scanner;
+import java.io.File;  // Import the File class
 
 public class Client {
     public static void main(String[] args) {
         // Ensure the correct number of arguments are provided
-        if (args.length != 2) {
-            System.out.println("Usage: java Client <hostname> <port>");
+        if (args.length != 3) {
+            System.out.println("Usage: java Client <hostname> <port> <filename>");
             return;
         }
 
         String hostname = args[0];
         int port = Integer.parseInt(args[1]);
+        String filename = args[2];
+        File downloadFile = new File(filename);
 
         // Create a UDP socket
         try (DatagramSocket clientSocket = new DatagramSocket()) {
+
             System.out.println("Client is ready to send data.");
-            Scanner scanner = new Scanner(System.in);
-            // Input a date from the user
-            System.out.print("Enter a date (YYYY-MM-DD): ");
-            String date = scanner.nextLine();
 
+            // Request to download files from filename
+            Scanner scanner = new Scanner(downloadFile);
+           
+            while (scanner.hasNextLine()) {
+                // Get file to download
+                String file = scanner.nextLine();
+                String request = String.format("DOWNLOAD %s", file);
 
-            
-            // Send the date to the server
-            byte[] sendData = date.getBytes();
-            InetAddress serverAddress = InetAddress.getByName(hostname);
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, 
-                    serverAddress, port);
-            clientSocket.send(sendPacket);
-            System.out.println("Date sent to the server.");
+                // Send download request to the server
+                byte[] sendData = request.getBytes();
+                InetAddress serverAddress = InetAddress.getByName(hostname);
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, port);
+                clientSocket.send(sendPacket);
 
+                System.out.println("File sent to the server.");
 
+                // Receive the response from the server
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                clientSocket.receive(receivePacket);
 
-            // Receive the response (weekday) from the server
-            byte[] receiveData = new byte[1024];
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
+                String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Server response: " + response);
+            }
 
-            String response = new String(receivePacket.getData(), 0, 
-                receivePacket.getLength());
-            System.out.println("Server response: " + response);
+            scanner.close();
 
         } catch (Exception e) {
             System.out.println("Client error: " + e.getMessage());
