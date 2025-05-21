f3785d9d9892653819ef81f2bc727b54ae34b37e
cw500 <cw500@students.waikato.ac.nz>
2025-05-22 07:06:37 +1200

    Added Download Function
    Will loop and download a file from the server

--- Client.java
+++ Client.java
@@ -1,6 +1,7 @@
 import java.net.*;
 import java.util.Scanner;
-import java.io.File;  // Import the File class
+import java.io.*;  // Import the File class
+import java.util.Base64;
 
 public class Client {
     public static void main(String[] args) {
@@ -32,7 +33,13 @@ public class Client {
                 sendRequest(request, clientSocket, port);
 
                 // Receive the response from the server
-                getResponse(clientSocket);
+                String response = getResponse(clientSocket);
+                String[] dataArr = response.split(" ");
+                int size = Integer.parseInt(dataArr[3]);
+                port = Integer.parseInt(dataArr[5]);
+
+                // Depending on response go download the file
+                downloadFile(file, size, clientSocket, port);
             }
 
             scanner.close();
@@ -49,21 +56,61 @@ public class Client {
             InetAddress serverAddress = InetAddress.getByName(hostname);
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, port);
             clientSocket.send(sendPacket);
-
-            System.out.println("Request sent to the server.");
+            System.out.println("Request sent to server: " + request);
         }
         catch(Exception e){System.out.println(e);}
     }
 
-    public static void getResponse(DatagramSocket clientSocket){
+    public static String getResponse(DatagramSocket clientSocket){
         try{
-            byte[] receiveData = new byte[1024];
+            byte[] receiveData = new byte[1000];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket);
             String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
 
-            System.out.println("Server response: " + response);
+            System.out.println("Response from server " + response);
+            return response;
         }
         catch(Exception e){System.out.println(e);}
+
+        return null;
+    }
+
+
+    private static void downloadFile(String file, int size, DatagramSocket client, int port){
+        int bytesToRead = 1000;
+        int start = 0;
+        int end = bytesToRead;
+
+        String request;
+
+        while(start < size){
+            // Send request 
+            request = String.format("FILE %s GET START %d END %d", file, start, end);
+            sendRequest(request, client, port);
+        
+            // Get response
+            String response = getResponse(client);
+            String[] dataArr = response.split(" ");
+            //String content = dataArr[8];
+
+            // decode into String from encoded format
+            byte[] actualByte = Base64.getDecoder().decode(dataArr[8]);
+            String content = new String(actualByte);
+
+            // Write to file
+            try (RandomAccessFile randFile = new RandomAccessFile("COPY" + file, "rw")) {
+                randFile.seek(start);
+                randFile.writeBytes(content);
+            }
+            catch(Exception e){}
+
+            // Increment start and end
+            start = end;
+            end +=  bytesToRead;
+        }
+
+        request = String.format("FILE %s CLOSE", file);
+        sendRequest(request, client, port);
     }
 }
